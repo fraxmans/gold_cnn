*fix wrong index of closing price during loading data *change directory name for saving/accesing pics/data. *reduce epoch num

src/gold_forex_multi_plot.py
@@ -21,11 +21,11 @@ idx_close = 1
 
 def load_raw_data():
     gold_raw_data = np.loadtxt("data/XAUUSD_small.csv", delimiter=" ")
-    gold_raw_data = gold_raw_data[:, [0, -2]]
+    gold_raw_data = gold_raw_data[:, [0, -1]]
     gold_raw_data = remove_duplicate(gold_raw_data)
 
     forex_raw_data = np.loadtxt("data/EURUSD_small.csv", delimiter=" ")
-    forex_raw_data = forex_raw_data[:, [0, -2]]
+    forex_raw_data = forex_raw_data[:, [0, -1]]
     forex_raw_data = remove_duplicate(forex_raw_data)
 
     return gold_raw_data, forex_raw_data

src/make_data.py
@@ -19,7 +19,7 @@ idx_close = 1
 
 def load_raw_data():
     gold_raw_data = np.loadtxt("data/XAUUSD.csv", delimiter=" ")
-    gold_raw_data = gold_raw_data[:, [0, -2]]
+    gold_raw_data = gold_raw_data[:, [0, -1]]
     gold_raw_data = remove_duplicate(gold_raw_data)
 
     return gold_raw_data

src/multi_plot.py
@@ -13,7 +13,7 @@ from functools import partial
 
 seq_len = int(sys.argv[1])
 label_num = int(sys.argv[2])
-kind = "raw"
+kind = "raw-avg"
 prefix = "%s/%dmin" % (kind, seq_len)
 
 idx_time = 0
@@ -21,7 +21,7 @@ idx_close = 1
 
 def load_raw_data():
     gold_raw_data = np.loadtxt("data/XAUUSD.csv", delimiter=" ")
-    gold_raw_data = gold_raw_data[:, [0, -2]]
+    gold_raw_data = gold_raw_data[:, [0, -1]]
     gold_raw_data = remove_duplicate(gold_raw_data)
 
     return gold_raw_data